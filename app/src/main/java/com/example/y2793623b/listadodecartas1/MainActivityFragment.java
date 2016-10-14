32ff37bad0722062adeb7e39df24ee219cd89f09
karim <y2793623b@iespoblenou.org>
2016-10-14 17:33:55 +0200

    Crear el layout nuevo que tiene texto del nom de la carta

--- app/src/main/java/com/example/y2793623b/listadodecartas1/MainActivityFragment.java
+++ app/src/main/java/com/example/y2793623b/listadodecartas1/MainActivityFragment.java
@@ -2,14 +2,27 @@ package com.example.y2793623b.listadodecartas1;
 
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.support.v4.widget.ListViewAutoScrollHelper;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
+
+import java.lang.reflect.Array;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.StringTokenizer;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class MainActivityFragment extends Fragment {
+
+
+    private ArrayList<String> Listcartas;
+    private ArrayAdapter<String> adapter;
 
     public MainActivityFragment() {
     }
@@ -17,6 +30,23 @@ public class MainActivityFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_main, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_main, container, false);
+
+        ListView lvCartas = (ListView) view.findViewById(R.id.LvCartas);
+
+        String[] cartas = {"carta1" , "carta2" , "carta3" , "carta4" , "carta5"};
+
+        Listcartas = new ArrayList<>(Arrays.asList(cartas));
+
+        adapter = new ArrayAdapter<String>(
+                getContext(),
+                R.layout.lv_cartas_row,
+                R.id.txt_NameCard,
+                Listcartas
+        );
+
+
+        return view;
     }
 }
