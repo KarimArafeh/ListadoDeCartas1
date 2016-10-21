78853506b73ae54a71062ed67e35d25747229cbc
karim <y2793623b@iespoblenou.org>
2016-10-21 15:42:17 +0200

    Reconfigurar : tenia que crear un menu nuevo para mostrar el button de refrescar pero lo que hecho es modificar ek menu original (lo del button setting) entonces me sale (Refresh) dos veces por eso tengo que comentrar la llamada del menu en el fichero "MainActivity"

--- app/src/main/java/com/example/y2793623b/listadodecartas1/MainActivity.java
+++ app/src/main/java/com/example/y2793623b/listadodecartas1/MainActivity.java
@@ -18,16 +18,8 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
     }
-
+    /*
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
@@ -43,10 +35,10 @@ public class MainActivity extends AppCompatActivity {
         int id = item.getItemId();
 
         //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
+        if (id == R.id.action_refresh) {
             return true;
         }
 
         return super.onOptionsItemSelected(item);
-    }
+    }*/
 }
