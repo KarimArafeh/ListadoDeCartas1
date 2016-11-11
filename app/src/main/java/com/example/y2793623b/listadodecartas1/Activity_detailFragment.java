91ab1076b18d51dfb36dd566f71ed3e0948e45f2
karim <y2793623b@iespoblenou.org>
2016-11-11 18:28:57 +0100

    Agafem els views del layout en el fragment

--- app/src/main/java/com/example/y2793623b/listadodecartas1/Activity_detailFragment.java
+++ app/src/main/java/com/example/y2793623b/listadodecartas1/Activity_detailFragment.java
@@ -1,17 +1,31 @@
 package com.example.y2793623b.listadodecartas1;
 
 import android.content.Intent;
+import android.media.Image;
+import android.provider.MediaStore;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 /**
  * A placeholder fragment containing a simple view.
  */
 public class Activity_detailFragment extends Fragment {
+
+    private View view;
+    private ImageView cartaImage;
+    private TextView cartaName;
+    private TextView cartaColor;
+    private TextView cartaSet;
+    private TextView cartaText;
+    private TextView cartaRarity;
+
+
 
     public Activity_detailFragment() {
     }
@@ -20,7 +34,7 @@ public class Activity_detailFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
 
-        View view = inflater.inflate(R.layout.fragment_activity_detail, container, false);
+        view = inflater.inflate(R.layout.fragment_activity_detail, container, false);
 
         Intent i = getActivity().getIntent();
 
@@ -42,5 +56,13 @@ public class Activity_detailFragment extends Fragment {
     private void updateUi(Card carta)
     {
         Log.d("CARD", carta.toString());
+
+        cartaImage = (ImageView) view.findViewById(R.id.CartaImage);
+        cartaName = (TextView) view.findViewById(R.id.CartaName);
+        cartaColor = (TextView) view.findViewById(R.id.CartaColor);
+        cartaSet = (TextView) view.findViewById(R.id.CartaSet);
+        cartaRarity = (TextView) view.findViewById(R.id.CartaRarity);
+        cartaText = (TextView) view.findViewById(R.id.CartaText);
+
     }
 }
