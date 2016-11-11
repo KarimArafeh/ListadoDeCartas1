1b4cb32c22007c57816d382a6db5bff4eee606ea
karim <y2793623b@iespoblenou.org>
2016-11-11 18:05:46 +0100

    agafem les dades en el Fragment de detal i les mostrem

--- app/src/main/java/com/example/y2793623b/listadodecartas1/Activity_detailFragment.java
+++ app/src/main/java/com/example/y2793623b/listadodecartas1/Activity_detailFragment.java
@@ -1,7 +1,9 @@
 package com.example.y2793623b.listadodecartas1;
 
+import android.content.Intent;
 import android.support.v4.app.Fragment;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -17,6 +19,28 @@ public class Activity_detailFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_activity_detail, container, false);
+
+        View view = inflater.inflate(R.layout.fragment_activity_detail, container, false);
+
+        Intent i = getActivity().getIntent();
+
+        if(i != null)
+        {
+            Card carta = (Card) i.getSerializableExtra("carta");
+
+            if(carta != null)
+            {
+                updateUi(carta);
+            }
+
+        }
+
+
+        return view;
+    }
+
+    private void updateUi(Card carta)
+    {
+        Log.d("CARD", carta.toString());
     }
 }
