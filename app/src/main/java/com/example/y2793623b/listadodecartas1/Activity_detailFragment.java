0c3c9a5f71d13bb07c9e4d26052d89a647c0d283
karim <y2793623b@iespoblenou.org>
2016-11-11 17:31:53 +0100

    Creem l'activitat de detall

---
+++ app/src/main/java/com/example/y2793623b/listadodecartas1/Activity_detailFragment.java
@@ -0,0 +1,22 @@
+package com.example.y2793623b.listadodecartas1;
+
+import android.support.v4.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * A placeholder fragment containing a simple view.
+ */
+public class Activity_detailFragment extends Fragment {
+
+    public Activity_detailFragment() {
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.fragment_activity_detail, container, false);
+    }
+}
