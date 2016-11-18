b9c0e8f7d6642de07e2407857ee691d917ce19e1
karim <y2793623b@iespoblenou.org>
2016-11-18 16:33:35 +0100

    Fem el mataix en l'adapter

--- app/src/main/java/com/example/y2793623b/listadodecartas1/CartasAdapter.java
+++ app/src/main/java/com/example/y2793623b/listadodecartas1/CartasAdapter.java
@@ -2,6 +2,7 @@ package com.example.y2793623b.listadodecartas1;
 
 import android.content.Context;
 import android.support.annotation.NonNull;
+import android.databinding.DataBindingUtil;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -11,6 +12,7 @@ import android.widget.ImageView;
 import android.widget.TextView;
 
 import com.bumptech.glide.Glide;
+import com.example.y2793623b.listadodecartas1.databinding.LvCartasRowBinding;
 
 import org.w3c.dom.Text;
 
@@ -36,29 +38,36 @@ public class CartasAdapter extends ArrayAdapter<Card> {
 
         // Obtenim l'objecte en la possició corresponent
         Card carta = getItem(position);
-        Log.w("xx", carta.toString());
+        Log.w(" card >>  ", carta.toString());
+
+        LvCartasRowBinding binding = null;
 
         if(convertView == null)
         {
             LayoutInflater inflater = LayoutInflater.from(getContext());
-            convertView = inflater.inflate(R.layout.lv_cartas_row, parent, false);
+            //convertView = inflater.inflate(R.layout.lv_cartas_row, parent, false);
+            binding = DataBindingUtil.inflate(inflater, R.layout.lv_cartas_row, parent, false);
+        } else {
+            binding = DataBindingUtil.getBinding(convertView);
         }
 
+        /*
         //unim el codi en les views del Layout
         TextView cartaNom = (TextView) convertView.findViewById(R.id.CartaNom);
 
         //TextView cartaColor = (TextView) convertView.findViewById(R.id.CartaColor);
         ImageView cartaImage = (ImageView) convertView.findViewById(R.id.CartaImage);
-
         // Fiquem les dades dels objectes (provinents del JSON) en el layout
         cartaNom.setText(carta.getName());
         //cartaColor.setText(carta.getColors());
-
         Glide.with(getContext()).load(carta.getImageUrl()).into(cartaImage);
-
+        */
+        binding.CartaNom.setText(carta.getName());
+        Glide.with(getContext()).load(carta.getImageUrl()).into(binding.CartaImage);
 
         // Retornem la View replena per a mostrarla
-        return convertView;
+        //return convertView;
+        return binding.getRoot();
 
     }
 }
